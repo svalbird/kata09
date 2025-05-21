c29979467ebd81c89d07abb5e9021ca79051e1d9
Matthew Randle <121844052+svalbird@users.noreply.github.com>
2025-05-21 12:16:58 +1200

    added more descriptive test notes

--- test.py
+++ test.py
@@ -16,11 +16,11 @@ class TestCheckout(unittest.TestCase):
         
         self.pricing_rules = [
             PricingRule(3, "A", "A", 30, False),  # 3 for 130 instead of 150
-            PricingRule(2, "B", "B", 15, False)    # 2 for 45 instead of 60
+            PricingRule(2, "B", "B", 15, False)   # 2 for 45 instead of 60
         ]
     
     def price(self, goods: str) -> float:
-
+        #Helper function to test price combos
         co = CheckOut(self.pricing_rules)
         for item_name in goods:
             if item_name == "A":
@@ -34,7 +34,7 @@ class TestCheckout(unittest.TestCase):
         return co.get_total_cost()
     
     def test_totals(self):
-
+        #test that total are being added correctly, regardless of combination
         self.assertEqual(0, self.price(""))
         self.assertEqual(50, self.price("A"))
         self.assertEqual(80, self.price("AB"))
@@ -51,7 +51,7 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(190, self.price("DBBAAA"))
     
     def test_incremental(self):
-
+        #test that totals are valid at each scan
         co = CheckOut(self.pricing_rules)
         self.assertEqual(0, co.get_total_cost())
         
@@ -71,7 +71,7 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(175, co.get_total_cost())
     
     def test_unscan(self):
-
+        #test that items can be unscanned
         co = CheckOut(self.pricing_rules)
         
         co.scan(self.A)
@@ -86,15 +86,14 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(80, co.get_total_cost())  # Should be A + B = 80
     
     def test_unscan_error(self):
-
+        #Test error on unscanning items
         co = CheckOut(self.pricing_rules)
 
-        #Test error
         with self.assertRaises(ValueError):
             co.unscan(self.C)
     
     def test_discount_limits(self):
-
+        #test discounts with limits to ensure that they're only being applied a valid number of times
         limited_rules = [
             PricingRule(3, "A", "A", 30, 1),  # Limit to one set of 3
             PricingRule(2, "B", "B", 15, 2)   # Limit to two sets of 2
@@ -106,7 +105,6 @@ class TestCheckout(unittest.TestCase):
             co.scan(self.A)
         self.assertEqual(280, co.get_total_cost())
         
-        
         for _ in range(6):
             co.scan(self.B)
         self.assertEqual(430, co.get_total_cost())
@@ -115,29 +113,29 @@ class TestCheckout(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 
-def test_checkout_output():
-        pricing_rules = [
-            PricingRule(3, "A", "A", 30.0, False),  # 3 for 130 instead of 150
-            PricingRule(2, "B", "B", 15.0, False),    # 2 for 45 instead of 60
-            PricingRule(1, "C", "C", 17.0, False)
-        ]
+# def test_checkout_output():
+#         pricing_rules = [
+#             PricingRule(3, "A", "A", 30.0, False), 
+#             PricingRule(2, "B", "B", 15.0, False),  
+#             PricingRule(1, "C", "C", 17.0, False)
+#         ]
 
-        A = StoreItem("A", 50.0)
-        B = StoreItem("B", 30.0)
-        C = StoreItem("C", 20.0)
-        D = StoreItem("D", 15.0)
+#         A = StoreItem("A", 50.0)
+#         B = StoreItem("B", 30.0)
+#         C = StoreItem("C", 20.0)
+#         D = StoreItem("D", 15.0)
 
-        co = CheckOut(pricing_rules)
-        co.scan(A)
-        co.scan(A)
-        co.scan(B)
-        co.scan(A)
-        co.scan(B)
-        co.scan(C)
-        co.scan(C)
-        co.scan(D)
-        co.scan(A)
+#         co = CheckOut(pricing_rules)
+#         co.scan(A)
+#         co.scan(A)
+#         co.scan(B)
+#         co.scan(A)
+#         co.scan(B)
+#         co.scan(C)
+#         co.scan(C)
+#         co.scan(D)
+#         co.scan(A)
 
-        co.get_item_summary()
+#         co.get_item_summary()
 
 #test_checkout_output()
